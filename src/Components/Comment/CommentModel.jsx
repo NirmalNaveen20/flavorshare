20deb0ded7ef54cb3a10a3ed1b055582c86fdccb
IT21037788 <it21037788@my.sliit.lk>
2023-04-12 00:12:43 +0530

    Update comment section

--- src/Components/Comment/CommentModel.jsx
+++ src/Components/Comment/CommentModel.jsx
@@ -1,19 +1,30 @@
 import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
 import React from 'react'
+import { BsThreeDots } from 'react-icons/bs'
 
 const CommentModel = ({onClose, isOpen}) => {
   return (
     <div>
-        <Modal onClose={onClose} isOpen={true} isCentered>
+        <Modal size={"4xl"} onClose={onClose} isOpen={true} isCentered>
         <ModalOverlay />
         <ModalContent>
           <ModalBody>
-          <div>
+          <div className='flex h-[75vh]'>
             <div className='w-[45%] flex flex-col justify-center'>
                 <img className='max-h-full w-full' src='https://cdn.pixabay.com/photo/2023/03/28/01/10/money-7881948__340.jpg' alt='' />
             </div>
-            <div className='border w-[55%]'> 
-                
+            <div className='w-[55%] pl-10'> 
+                <div className='flex justify-between items-center py-5'>
+                    <div className='flex items-center'>
+                    <div>
+                        <img className='w-9 h-9 rounded-full' src='https://cdn.pixabay.com/photo/2022/06/16/13/02/dog-7265934__340.jpg' alt='' />
+                    </div>
+                    <div className='ml-2'>
+                        <p>username</p>
+                    </div>
+                </div>
+                    <BsThreeDots />
+                </div>                    
             </div>
           </div>
           </ModalBody>
