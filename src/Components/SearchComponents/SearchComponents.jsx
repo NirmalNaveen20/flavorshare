10f55d76010a4d46aacdf9edbc921b30ee0c7023
IT21037788 <it21037788@my.sliit.lk>
2023-04-14 11:55:35 +0530

    Update README

---
+++ src/Components/SearchComponents/SearchComponents.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./SearchComponents.css"
+
+const SearchComponents = () => {
+  return (
+    <div className='searchContainer'>
+        <div className='px-3 pb-5'>
+              <h1 className='text-xl pb-5'>Search</h1>
+              <input className='searchInput' type='text' placeholder='Search...' />
+          </div>
+          <hr />
+
+          <div>
+              
+          </div>
+    </div>
+  )
+}
+
+export default SearchComponents
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,11 +4,13 @@ import { mainu } from './SidebarConfig'
 import { useNavigate } from 'react-router-dom';
 import CreatePostModel from '../Post/CreatePostModel';
 import { useDisclosure } from '@chakra-ui/react';
+import SearchComponents from '../SearchComponents/SearchComponents';
 
 const Sidebar = () => {
-  const [activeTab, setActiveTab] = useState();
+  const [activeTab, setActiveTab] = useState("");
   const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
 
     const handleTabClick = (title) => {
         setActiveTab(title);
@@ -23,32 +25,37 @@ const Sidebar = () => {
         else if (title === "Create") {
           onOpen();
       }
+      if (title === "Search") {
+        setIsSearchVisible(true)
+      }
+      else setIsSearchVisible(false)
     };
     
 
   return (
-    <div className='sticky top-0 h-[100vh]'>
-          <div className='flex flex-col justify-between h-full px-10'>
-              <div>
+    <div className='sticky top-0 h-[100vh] flex'>
+          <div className={`flex flex-col justify-between h-full ${activeTab==="Search"?"px-2":"px-10"}`}>
+              {<div>
                   
-            <div className='pt-10'>
+            {activeTab!=="Search" && <div className='pt-10'>
                 <img className='w-40' src='https://i.imgur.com/zqpwkLQ.png' alt='' />
-              </div>
+              </div>}
               <div className='mt-10'>
                   {mainu.map((item) => (
                   <div onClick={()=>handleTabClick(item.title)} className='flex items-center mb-5 cursor-pointer text-lg'>
                     {activeTab===item.title? item.activeIcon:item.icon}    
-                      <p className={`${activeTab===item.title?"font-bold":"font-semibold"}`}>{ item.title }</p>                      
+                      {activeTab !== "Search" && <p className={`${activeTab===item.title?"font-bold":"font-semibold"}`}>{ item.title }</p>}                      
                   </div>
                   ))}                  
                 </div>
-              </div>
+              </div>}
               <div className='flex items-center cursor-pointer pb-10'> 
               <IoReorderThreeOutline className="text-2xl" />
-              <p className='ml-5'>More</p>
+              {activeTab !== "Search" && <p className='ml-5'>More</p>}
           </div>
       </div>
       <CreatePostModel onClose={onClose} isOpen={isOpen} />
+      {isSearchVisible && <SearchComponents />}
       </div>
       
   )
