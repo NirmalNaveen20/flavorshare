c2132c66c0dcb5b157b716209f5f719ebbc96496
IT21037788 <it21037788@my.sliit.lk>
2023-05-02 11:20:16 +0530

    Add register component

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Auth.css";
+
+export const Auth = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div className=" h-[35.3rem] w-[23rem]">
+            <img
+              className="h-full w-full"
+              src="https://res.cloudinary.com/dnbw04gbs/image/upload/v1679494375/home-phones-2x-edited_glksxn.png"
+              alt="FavorFeed Homepage"
+            />
+            <div className="mobileWallpaper rounded-3xl absolute top-6 h-[33rem] w-[15.7rem] w- right-3">
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,27 +1,58 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from '../HomePage/HomePage'
-import Profile from '../Profile/Profile'
-import Story from '../Story/Story'
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
-const Router = () => {
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import HomePage from "../HomePage/HomePage";
+import Profile from "../Profile/Profile";
+import Story from "../Story/Story";
+import { Auth } from "../Auth/Auth";
+
+const Routers = () => {
+  const location =useLocation();
   return (
     <div>
-        <div className='flex'>
-            <div className='w-[20%] border border-1-slate-500'>
-                <Sidebar />
-            </div>
-            <div className='w-full'>
-                <Routes>
-                      <Route path='/' element={<HomePage />}></Route>
-                      <Route path='/username' element={<Profile />}></Route>
-                      <Route path='/story' element={<Story />}></Route>
-                </Routes>
-            </div>
-        </div>
+      {/* <div className="flex ">
+      <div className="sidebarBox border border-l-slate-500 w-[20%]">
+        <Sidebar />
+      </div>
+      
+      <div className="w-[80%] ">
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/p/:postId" element={<HomePage />}></Route>
+          <Route path="/:username" element={<Profile />}></Route>
+          <Route path="/demo" element={<StoryPage />}></Route>
+          <Route path="/story/:userId" element={<Story />}></Route>
+          <Route path="/account/edit" element={<EditProfilePage />}></Route>
+        </Routes>
+      </div>
     </div>
-  )
-}
+     */}
 
-export default Router
+{(location.pathname !== "/login" && location.pathname!=="/signup")&& (
+    <div className="flex">
+      <div className="sidebarBox border border-l-slate-500 w-[20%]">
+        <Sidebar />
+      </div>
+      <div className="w-[80%]">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/p/:postId" element={<HomePage />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/story/:userId" element={<Story />} />
+        </Routes>
+      </div>
+    </div>
+  )}
+  {(location.pathname === "/login" || location.pathname==="/signup") && (
+    <Routes>
+      <Route path="/login" element={<Auth />} />
+      <Route path="/signup" element={<Auth />} />
+    </Routes>
+  )}
+    </div>
+    
+  );
+};
+
+export default Routers;
